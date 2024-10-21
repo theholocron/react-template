c933125ac8f5a5d4b0bf577720b3524a03f14b31
Newton <5769156+iamnewton@users.noreply.github.com>
2024-10-21 15:12:05 -0700

    chore: 🔧  update vite configuration

--- src/button/button.story.ts
+++ src/button/button.story.ts
@@ -7,12 +7,6 @@ import { Button } from "./button";
 const meta: Meta<typeof Button> = {
 	title: "Example/Button",
 	component: Button,
-	parameters: {
-		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-		layout: "centered",
-	},
-	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-	tags: ["autodocs"],
 	// More on argTypes: https://storybook.js.org/docs/api/argtypes
 	argTypes: {
 		backgroundColor: { control: "color" },
--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,29 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'), // Entry point of your library
-      name: 'react-template',
-      formats: ['es', 'cjs'], // Specify formats (ESM and CommonJS)
-      fileName: (format) => `react-template.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'], // Externalize peer dependencies
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Example alias, adjust as needed
-    },
-  },
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'), // Entry point of your library
+			name: 'react-template',
+			formats: ['es', 'cjs'], // Specify formats (ESM and CommonJS)
+			fileName: (format) => `react-template.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'], // Externalize peer dependencies
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"), // Example alias, adjust as needed
+		},
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom', // Use jsdom for testing React components
