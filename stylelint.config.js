1f039194aa005791de00ae57e6e7edf8eb9c6743
Newton <5769156+iamnewton@users.noreply.github.com>
2024-10-24 14:31:24 -0700

    chore: 🚨 fix linter complaints
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,9 @@
-import theholocron from "@theholocron/stylelint-config";
-
 /**
  * @see https://stylelint.io/user-guide/configure/
  * @type {import("stylelint").Config}
  */
 const config = {
-	extends: [...theholocron],
+	extends: ["@theholocron"],
 };
 
 export default config;
