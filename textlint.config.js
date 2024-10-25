b759a843b3197870700b8ffb3dfe80a7e96c625e
Newton <5769156+iamnewton@users.noreply.github.com>
2024-10-25 14:34:24 -0700

    chore: 🚨 fix textlint errors
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

---
+++ textlint.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://textlint.github.io/docs/configuring.html
+ * @type {import("@textlint/types").TextlintConfig}
+ */
+const config = {
+	filters: {
+		comments: true,
+	},
+};
+
+export default config;
