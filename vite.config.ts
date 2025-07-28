9555065124afbcc8c17d747f7f72cd79c72a66c0
Newton <5769156+iamnewton@users.noreply.github.com>
2025-07-28 13:39:28 -0700

    chore: fix storybook test action
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
 						// This should match your package.json script to run Storybook
 						// The --ci flag will skip prompts and not open a browser
 						storybookScript: "npm start --ci",
+						storybookUrl: process.env.SB_URL,
 					}),
 				],
 				test: {
