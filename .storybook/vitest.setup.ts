e3bad87301e39f11277f36cdb803b9387ccfff4c
Newton <5769156+iamnewton@users.noreply.github.com>
2025-07-29 17:50:34 -0700

    chore: fix location for storybook
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,10 +1,9 @@
 import "@testing-library/jest-dom";
-import { afterAll, afterAll, beforeAll } from "vitest";
-import { server } from "../app/mocks/node";
-
 // 👇 If you're using Next.js, import from @storybook/nextjs
 //   If you"re using Next.js with Vite, import from @storybook/experimental-nextjs-vite
 import { setProjectAnnotations } from "@storybook/react";
+import { afterAll, afterAll, beforeAll } from "vitest";
+import { server } from "../app/mocks/node.ts";
 // 👇 Import the exported annotations, if any, from the addons you're using; otherwise remove this
 // import * as addonAnnotations from "my-addon/preview";
 import * as previewAnnotations from "./preview";
