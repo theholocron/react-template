2da0ca520874a499d347890f934c42fec59f0a70
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-04 08:44:37 -0700

    chore: remove redundant JSDoc from stylelint config
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from "stylelint";
 
-/**
- * @see https://stylelint.io/user-guide/configure/
- */
 export default {
 	extends: ["@theholocron/stylelint-config"],
 } satisfies Config;
