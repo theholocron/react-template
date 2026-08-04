95a866f426af149af5c6f3fb09b981e2f6c383fa
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-04 08:16:50 -0700

    chore: upgrade to @theholocron 7.11.0 and simplify vite.config
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
-import { fileURLToPath } from "node:url";
-
 import { reactLibrary } from "@theholocron/vite-config/react-library";
 
 /*
  * @see https://vitejs.dev/config/
  */
-export default reactLibrary({
-	name: "react-template",
-	overrides: {
-		publicDir: "public",
-		resolve: {
-			alias: {
-				"@": fileURLToPath(new URL("./src", import.meta.url)),
-			},
-		},
-	},
-});
+export default reactLibrary({ name: "react-template" });
