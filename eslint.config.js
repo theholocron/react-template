803cf8ce8df7172537fb3d324d630cf259b56a41
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-03 22:31:05 -0700

    chore: use vitest-config MSW setup helpers and update all deps
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,18 @@
-import cypress from "@theholocron/eslint-config/cypress.eslint.config.js";
-import holocron from "@theholocron/eslint-config/eslint.config.js";
-import storybook from "@theholocron/eslint-config/storybook.eslint.config.js";
+import { reactApp } from "@theholocron/eslint-config/bundles/react-app";
+import { cypress } from "@theholocron/eslint-config/cypress";
 
 /**
  * @see https://eslint.org/docs/latest/use/configure/
  * @type {import("eslint").Linter.Config}
  */
-const config = [...holocron, ...storybook, ...cypress];
+const config = [
+	...reactApp(),
+	...cypress(),
+	{
+		settings: {
+			react: { version: "detect" },
+		},
+	},
+];
 
 export default config;
