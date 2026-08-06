0dd6216203d1da83b2309bdab9acbdf83530ea80
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-05 21:22:34 -0700

    refactor: simplify vitest config using updated storybook() preset from vitest-config 7.14.4
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,31 +1,15 @@
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import { coverage } from "@theholocron/vitest-config";
-import { playwright } from "@vitest/browser-playwright";
+import { coverage, storybook } from "@theholocron/vitest-config";
 import { defineConfig } from "vitest/config";
 
 /*
  * @see https://vitest.dev/config/
  */
-export default defineConfig({
+export default defineConfig(async () => ({
 	test: {
 		coverage: {
 			...coverage,
 			exclude: [...coverage.exclude, "**/handlers.*", "**/*.{mock}.*", "**/*.css"],
 		},
-		projects: [
-			{
-				plugins: [storybookTest({ configDir: ".storybook" })],
-				test: {
-					name: "storybook",
-					browser: {
-						enabled: true,
-						headless: true,
-						provider: playwright({}),
-						instances: [{ browser: "chromium" }],
-					},
-					setupFiles: ["./vitest.setup.ts"],
-				},
-			},
-		],
+		projects: [await storybook(".storybook", { setupFiles: ["./vitest.setup.ts"] })],
 	},
-});
+}));
