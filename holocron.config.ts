43d02df5245af6c52d10cb8ae232b288b3332e78
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-14 14:01:50 -0700

    chore: change name
    Co-authored-by: Newton <5769156+iamnewton@users.noreply.github.com>
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- holocron.config.ts
+++ holocron.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 			},
 		},
 		{ name: "release", with: { "run-build": true } },
-		{ name: "deploy", with: { docs: true, storybook: [{ name: "sandbox" }] } },
+		{ name: "deploy", with: { docs: true, storybook: [{ name: "app" }] } },
 	],
 	providers: {
 		...providers,
